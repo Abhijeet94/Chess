c8e86f9d41fb6866902b3ced25ec99cbe15086ba
Draconix821 <romvillarica@gmail.com>
2017-11-16 16:16:24 -0500

    Starting files

---
+++ Tests.hs
@@ -0,0 +1,31 @@
+{-# OPTIONS_GHC -fwarn-tabs -fwarn-incomplete-patterns -fdefer-type-errors #-}
+module Tests where
+
+import Test.HUnit (runTestTT,Test(..),Assertion, (~?=), (~:), assert)
+import Test.QuickCheck (Arbitrary(..), Testable(..), Gen, elements,
+  oneof, frequency, sized, quickCheckWith, stdArgs, maxSize,
+  classify,  maxSuccess, listOf, resize, scale, (==>), choose)
+  
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+import Text.PrettyPrint (Doc)
+import qualified Text.PrettyPrint as PP
+
+import Control.Applicative (Alternative(..),liftA3)
+import Control.Monad (liftM, liftM2)
+
+import State (State)
+import qualified State as S
+
+import GameLogic
+
+-------------------------------------------------------------------------
+
+-- [QUESTION] How do we test this?
+-- [QUESTION] Arbitrary instances for Location, Piece, Board?
+instance Arbitrary Location where
+  arbitrary = liftM2 Loc (choose (1,8)) (choose (1,8))
+  shrink (Loc x y)       = []
+-- [QUESTION] Is QuickCheck even worth it in this case because all the cases are incredibly complex
+
