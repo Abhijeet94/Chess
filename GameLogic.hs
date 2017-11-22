1cc1d4c6be40f91914b6f7b24a08235cab3102d1
Abhijeet <abhijeet060@gmail.com>
2017-11-22 17:45:15 -0500

    code compiling. Things left - condition for win, check; special cases like castling etc; IO

--- GameLogic.hs
+++ GameLogic.hs
@@ -22,7 +22,7 @@ data Piece = P Player PieceType deriving (Eq, Show)
 type Board = Map Location Piece
 data Move = Move {src :: Location, dest :: Location}
 data Game = Game { board :: Board, current :: Player }
-data End = Win Player | Tie
+data End = BlackWins | WhiteWins | Checked | Tie | Playing
 type ChessBoard = StateT Game (Either String)
 
 ------------------------------------------------------------------------- 
@@ -49,11 +49,34 @@ getPiece loc = do
 
 handleTurn :: Move -> ChessBoard ()
 handleTurn move = do
-                    (P cl pt) <- getPiece (src move)
+                    p@(P cl pt) <- getPiece (src move)
                     game <- S.get
                     if (current game) == cl
-                        then movePiece move
+                        then if (validMove p (src move) (dest move))
+                            then movePiece move
+                            else throwError $ "Invalid move for " ++ (show pt)
                         else throwError $ "It is " ++ (show $ current game) ++ "'s turn"
+
+-------------------------------------------------------------------------
+
+-- Takes in a Piece and a proposed Location and returns 
+-- whether or not the Piece can move to that Location 
+-- according to the rules for each piece
+validMove :: Piece -> Location -> Location -> Bool
+validMove (P _ King) (Loc x1 y1) (Loc x2 y2) = (dist <= 2 && dist > 0) 
+                                    where dist = (x1 - x2)^2 + (y1 - y2)^2
+validMove (P _ Queen) (Loc x1 y1) (Loc x2 y2) = validMove (P White Bishop) (Loc x1 y1) (Loc x2 y2) ||
+                                                validMove (P White Rook) (Loc x1 y1) (Loc x2 y2)
+validMove (P _ Bishop) (Loc x1 y1) (Loc x2 y2) = ((abs (x1 - x2)) == (abs (y1 - y2))) && (abs (x1 - x2)) > 0
+validMove (P _ Knight) (Loc x1 y1) (Loc x2 y2) =    (((abs (x1 - x2)) == 2) && ((abs (y1 - y2)) == 1)) ||
+                                                    (((abs (y1 - y2)) == 2) && ((abs (x1 - x2)) == 1))
+validMove (P _ Rook) (Loc x1 y1) (Loc x2 y2) = ((x1 == x2) && (abs (y1 - y2)) > 0) 
+                                            || ((y1 == y2) && (abs (x1 - x2)) > 0)
+validMove (P White Pawn) (Loc x1 y1) (Loc x2 y2) = ((x1 == x2) && ((y2 - y1 == 1) || ((y1 == 2) && (y2 - y1 == 2)))) ||
+                                                    (((abs (x1 - x2)) == 1) && (y2 - y1 == 1))
+validMove (P Black Pawn) (Loc x1 y1) (Loc x2 y2) = ((x1 == x2) && ((y2 - y1 == -1) || ((y1 == 7) && (y2 - y1 == -2)))) ||
+                                                    (((abs (x1 - x2)) == 1) && (y2 - y1 == -1))                                                    
+
 
 -------------------------------------------------------------------------
 
@@ -63,13 +86,11 @@ otherPlayer Black = White
 
 -------------------------------------------------------------------------
 
-updateBoard :: Piece -> Move -> ChessBoard Location
-updateBoard p move = undefined
-                        --do
-                        --game <- S.get
-                        --S.put $ Game (Map.insert (src move) Nothing (board game)) (current game)
-                        --S.put $ Game (Map.insert (dest move) p (board game)) (current game)
-                        --return (dest move)
+updateBoard :: Piece -> Move -> ChessBoard ()
+updateBoard p move = do
+                        game <- S.get
+                        S.put $ Game (Map.delete (src move) (board game)) (current game)
+                        S.put $ Game (Map.insert (dest move) p (board game)) (current game)
 
 -------------------------------------------------------------------------
 
@@ -81,13 +102,13 @@ movePiece move = do
                             S.put $ Game (board game) (otherPlayer (current game))
                         else do
                             nextImmLoc <- getNextImmLocation move
-                            nextLoc <- movePieceOnce $ Move (src move) nextImmLoc
-                            movePiece $ Move nextLoc (dest move)
+                            movePieceOnce $ Move (src move) nextImmLoc
+                            movePiece $ Move nextImmLoc (dest move)
 
 -------------------------------------------------------------------------
 
 
-movePieceOnce :: Move -> ChessBoard Location
+movePieceOnce :: Move -> ChessBoard ()
 movePieceOnce move = do
                         pSrc <- getPiece (src move)
                         game <- S.get
@@ -148,145 +169,35 @@ getImmNextLocation (P _ Queen) l1@(Loc x1 y1) l2@(Loc x2 y2) = Loc x' y'
 
 -------------------------------------------------------------------------
 
+-- look for checkmate cases / tie cases
+
+-- assuming that the king will never be captured - a checkmate case
+-- will be reported as win and the game should end there itself.
+-- so before making a move, check whether its a checkmate
+
+--checkForWin :: ChessBoard End
+checkForWin = undefined
+                --do
+                --game <- S.get
+                --return WhiteWins
 
 
+kingLocation :: Player -> ChessBoard (Maybe Location)
+kingLocation pl = do
+                game <- S.get
+                return $ Map.foldrWithKey (kingFunc) Nothing (board game)
+                where
+                    kingFunc loc (P pl' King) _ | pl == pl' = Just loc
+                    kingFunc _ _ prev = prev
+
+--isChecked :: Player -> ChessBoard Bool
+--isChecked pl = 
+--                where
+--                    myKing :: Location
 
 
+-------------------------------------------------------------------------
 
 
-
-
-
-
-----------------------------------------------------
-----------------------------------------------------
-----------------------OLD---------------------------
-----------------------------------------------------
-----------------------------------------------------
-
-
-
-
-
----- just gets a Piece from the Location
---getPiece :: Location -> ChessBoard Piece
---getPiece mp from = do
---                    p <- Map.lookup from mp
---                    p
-
-
----- makes the actual move
----- sets the current player after a move successfully completes
----- [QUESTION] how to check if the move was successfully made?
---handleTurn :: Move -> ChessBoard Game
---handleTurn game from to = undefined
---                        --if to' == to
---                        --    then if (current game) == White
---                        --            then (Just (Game newBoard Black))
---                        --            else (Just (Game newBoard White))
---                        --    else Nothing
---                        --    where
---                        --    (to', newBoard) = case (S.runStateT makeMove (board game)) of 
---                        --                        (IO (a, b)) -> (a, b)
-
---                        --    makeMove :: ChessBoard Location
---                        --    makeMove =  do
---                        --                to' <- movePiece from to
---                        --                return to'
-
-
----- look for checkmate cases / tie cases
----- if there are none, return Nothing
----- otherwise return Just Win White / Just Win Black / Just Tie
---checkForWin :: Board -> Maybe End
---checkForWin = undefined
-
----- Takes in a Location and uses getPiece to get the piece at the Location
----- updates it
----- If there is a piece at the target Location then capture it 
----- TODO - Handle special scenarios like Castling, en passant etc
---movePiece :: Location -> Location -> ChessBoard Location
---movePiece from to | from == to = return to
---movePiece from to | otherwise  =    do
---                                    initialBoard <-  S.get
---                                    case (getPiece initialBoard from) of 
---                                        Nothing -> return from 
---                                        (Just pc) -> do
---                                                        toImm <- movePieceOnce from (getImmNextLocation initialBoard pc from to)
---                                                        movePiece toImm to
----- if there is a problem, it might loop infinitely because the error is not propagated up
-
---movePieceOnce :: Location -> Location -> ChessBoard Location
---movePieceOnce from to = do
---                        initialBoard <- S.get
---                        case (getPiece initialBoard from) of
---                            Nothing -> return from
---                            (Just (P colorFrom pcFrom)) -> case (getPiece initialBoard to) of
---                                                            Nothing -> S.put (Map.insert from Nothing initialBoard) >>
---                                                                    S.put (Map.insert to (Just (P colorFrom pcFrom)) initialBoard) >>
---                                                                    return to
---                                                            (Just (P colorTo pcTo)) -> if (colorFrom == colorTo)
---                                                                then return from
---                                                                else S.put (Map.insert from Nothing initialBoard) >>
---                                                                    S.put (Map.insert to (Just (P colorFrom pcFrom)) initialBoard) >>
---                                                                    return to                                    
-
---getImmNextLocation :: Board -> Piece -> Location -> Location -> Location
---getImmNextLocation bd (P _ Bishop) l1@(Loc x1 y1) l2@(Loc x2 y2) = Loc x' y'
---                                                                where x' = if x2 > x1
---                                                                            then x1 + 1
---                                                                            else x1 - 1
---                                                                      y' = if y2 > y1
---                                                                            then y1 + 1
---                                                                            else y1 - 1
-
---getImmNextLocation bd (P _ Rook) l1@(Loc x1 y1) l2@(Loc x2 y2) =  Loc x' y'
---                                                                where x' = if x2 > x1
---                                                                            then x1 + 1
---                                                                            else if x2 == x1
---                                                                                then x1
---                                                                                else x1 - 1
---                                                                      y' = if y2 > y1
---                                                                            then y1 + 1
---                                                                            else if y2 == y1
---                                                                                then y1
---                                                                                else y1 - 1                                                                                
---getImmNextLocation bd (P _ King) l1@(Loc x1 y1) l2@(Loc x2 y2) =  l2
---getImmNextLocation bd (P _ Knight) l1@(Loc x1 y1) l2@(Loc x2 y2) =  l2
---getImmNextLocation bd (P _ Pawn) l1@(Loc x1 y1) l2@(Loc x2 y2) =  if x1 /= x2
---                                                                    then l2
---                                                                    else if (abs (y2 - y1)) == 1
---                                                                        then l2
---                                                                        else Loc x1 (y1 + (y2-y1))
---getImmNextLocation bd (P _ Queen) l1@(Loc x1 y1) l2@(Loc x2 y2) = Loc x' y'
---                                                                where x' = if x2 > x1
---                                                                            then x1 + 1
---                                                                            else if x2 == x1
---                                                                                then x1
---                                                                                else x1 - 1
---                                                                      y' = if y2 > y1
---                                                                            then y1 + 1
---                                                                            else if y2 == y1
---                                                                                then y1
---                                                                                else y1 - 1                                                                        
-
----- Takes in a Piece and a proposed Location and returns 
----- whether or not the Piece can move to that Location 
----- according to the rules for each piece
---validMove :: Piece -> Location -> Location -> Bool
---validMove (P _ King) (Loc x1 y1) (Loc x2 y2) = (dist <= 2 && dist > 0) 
---                                    where dist = (x1 - x2)^2 + (y1 - y2)^2
---validMove (P _ Queen) (Loc x1 y1) (Loc x2 y2) = validMove (P White Bishop) (Loc x1 y1) (Loc x2 y2) ||
---                                                validMove (P White Rook) (Loc x1 y1) (Loc x2 y2)
---validMove (P _ Bishop) (Loc x1 y1) (Loc x2 y2) = ((abs (x1 - x2)) == (abs (y1 - y2))) && (abs (x1 - x2)) > 0
---validMove (P _ Knight) (Loc x1 y1) (Loc x2 y2) =    (((abs (x1 - x2)) == 2) && ((abs (y1 - y2)) == 1)) ||
---                                                    (((abs (y1 - y2)) == 2) && ((abs (x1 - x2)) == 1))
---validMove (P _ Rook) (Loc x1 y1) (Loc x2 y2) = ((x1 == x2) && (abs (y1 - y2)) > 0) 
---                                            || ((y1 == y2) && (abs (x1 - x2)) > 0)
---validMove (P White Pawn) (Loc x1 y1) (Loc x2 y2) = ((x1 == x2) && ((y2 - y1 == 1) || ((y1 == 2) && (y2 - y1 == 2)))) ||
---                                                    (((abs (x1 - x2)) == 1) && (y2 - y1 == 1))
---validMove (P Black Pawn) (Loc x1 y1) (Loc x2 y2) = ((x1 == x2) && ((y2 - y1 == -1) || ((y1 == 7) && (y2 - y1 == -2)))) ||
---                                                    (((abs (x1 - x2)) == 1) && (y2 - y1 == -1))                                                    
-
-
+-- handle special cases like castling, pawn conversion, en-passant etc
 
--- PlayChess.hs
+++ PlayChess.hs
@@ -1,99 +1,62 @@
 {-# OPTIONS_GHC -fwarn-tabs -fwarn-incomplete-patterns -fdefer-type-errors #-}
 module PlayChess where
+import Control.Monad.State (MonadState(..), StateT, State, runState, runStateT)
+import qualified Control.Monad.State as S    
 import GameLogic
 
+-------------------------------------------------------------------------
+
+main :: IO ()
+main = play
+
+play :: IO ()
+play = playGame initialGame
+
+-------------------------------------------------------------------------
+
+playGame :: Game -> IO ()
+playGame game = do
+                printBoard (board game)
+                case (checkForWin (board game)) of 
+                    WhiteWins -> putStrLn "White won."
+                    BlackWins -> putStrLn "Black won."
+                    Tie       -> putStrLn "Game tied."
+                    Checked   -> do
+                                    putStrLn "Check!"
+                                    continuePlay game
+                    Playing   -> continuePlay game
+
+                where
+
+                continuePlay :: Game -> IO ()
+                continuePlay game = do
+                    putStr $ "Player " ++ (show (current game)) ++ "'s turn: "
+                    input <- getLine
+                    case (getNextMove input) of
+                        Nothing -> do
+                                    putStrLn "Invalid input"
+                                    playGame game
+                        (Just move) -> case (runStateT (handleTurn move) game) of
+                                        Left s -> do
+                                                    putStrLn $ "Uh-oh: " ++ s
+                                                    playGame game
+                                        Right (_, game') -> do
+                                                            printBoard (board game')
+                                                            playGame game'
 
 
 
 
+-------------------------------------------------------------------------
+
+getNextMove :: String -> Maybe Move
+getNextMove = undefined
 
 
+-------------------------------------------------------------------------
 
+-- PrettyPrint our board
+printBoard :: Board -> IO ()
+printBoard = undefined  
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-----------------------------------------------------
-----------------------------------------------------
-----------------------OLD---------------------------
-----------------------------------------------------
-----------------------------------------------------
-
-
-
-
-
-
-
---getNextMove :: String
---getNextMove = undefined
-
----- White to move:
----- E4 E5
----- print board
----- Black to move:
-
----- takes in a string of format LetterNumber and returns the corresponding Location
----- this needs to be changed to get both of them! [TODO]
---inputToLocation :: String -> Maybe (Location,Location)
---inputToLocation (x:y:[]) = undefined --if the string is 2 characters then convert it appropriately
---inputToLocation _ = Nothing
-
----- takes in a string of format "E4 E5" and if this is a valid action:
----- updates board with movePiece
----- checks for win
----- prints board
----- sets turn to next player
----- else: 
----- ask for correct input
---playGame :: IO ()
---playGame game = do
---    printBoard (board game)
---    case (checkForWin (board game)) of 
---        (Just (Win White)) -> S.liftIO $ putStrLn "White wins"
---        (Just (Win Black)) -> S.liftIO $ putStrLn "Black wins"
---        (Just Tie) -> S.liftIO $ putStrLn "Tie. Game Over."
---        Nothing -> do
---            S.liftIO $ putStr $ "Player " ++ (show (current game)) ++ "'s turn"
---            case (inputToLocation (getNextMove)) of 
---                Nothing -> S.liftIO $ putStrLn "Incorrect input"
---                (Just (from@(Loc x1 y1), to@(Loc x2 y2))) -> do
---                        initialBoard <-  S.get
---                        case (getPiece initialBoard from) of 
---                            Nothing -> S.liftIO $ putStrLn "Incorrect input"
---                            (Just pc@(P pl _)) -> if ((validMove pc from to) && (pl == current game))
---                                then do
---                                    case (handleTurn game from to) of
---                                        (Just game') -> playGame game'
---                                        Nothing -> playGame game
---                                else S.liftIO $ putStrLn "Incorrect input"
-
-
----- PrettyPrint our board
---printBoard :: Board -> ChessBoard ()
---printBoard = undefined                                
+-------------------------------------------------------------------------
