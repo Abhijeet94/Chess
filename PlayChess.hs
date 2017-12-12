a3376b865d3b3576656af268aa8ab64086aff568
Abhijeet <abhijeet060@gmail.com>
2017-12-11 22:10:45 -0500

    added comments and validrange for input in playchess.hs

--- PlayChess.hs
+++ PlayChess.hs
@@ -18,72 +18,77 @@ play = playGame initialGame
 
 -------------------------------------------------------------------------
 
+-- Main entry point of the command line interface to Chess game.
+-- Drives all the IO and uses the GameLogic functions to play
+
 playGame :: (Input m, Output m) => Game -> m ()
 playGame game = do
-                    printBoard (board game)
-                    case (checkGameStatus game) of 
-                        WhiteWins -> T.write "White won.\n" 
-                        BlackWins -> T.write "Black won.\n" 
-                        Tie       -> T.write "Game tied.\n" 
-                        Checked   -> do
-                                        T.write "Check!\n" 
-                                        continuePlay game
-                        Playing   -> continuePlay game
+  printBoard (board game)
+  case (checkGameStatus game) of 
+      WhiteWins -> T.write "White won.\n" 
+      BlackWins -> T.write "Black won.\n" 
+      Tie       -> T.write "Game tied.\n" 
+      Checked   -> do
+                      T.write "Check!\n" 
+                      continuePlay game
+      Playing   -> continuePlay game
 
-                    where
+  where
 
-                    continuePlay :: (Input m, Output m) => Game -> m ()
-                    continuePlay game = do
-                        T.write ((show (current game)) ++ "'s turn: \n") 
-                        ms <- T.input
-                        case ms of
-                            Nothing -> return()
-                            (Just input) -> 
-                                if input == "exit" then return () else 
-                                if input == "printlog" then (printLog game) else do
-                                case (getNextMove input) of
-                                    Nothing -> do
-                                                T.write "Invalid input\n" 
-                                                playGame game 
-                                    (Just move) -> case (runStateT (handleTurn move) game) of
-                                                    Left "getPawnPromotionPiece" -> handlePawnPromotion game move
-                                                    Left s -> do
-                                                                T.write ("Uh-oh: " ++ s ++ "\n") 
-                                                                playGame game 
-                                                    Right (_, game') -> playGame game' 
+  continuePlay :: (Input m, Output m) => Game -> m ()
+  continuePlay game = do
+      T.write ((show (current game)) ++ "'s turn: \n") 
+      ms <- T.input
+      case ms of
+          Nothing -> return()
+          (Just input) -> 
+              if input == "exit" then return () else 
+              if input == "printlog" then (printLog game) else do
+              case (getNextMove input) of
+                  Nothing -> do
+                              T.write "Invalid input\n" 
+                              playGame game 
+                  (Just move) -> case (runStateT (handleTurn move) game) of
+                                  Left "getPawnPromotionPiece" -> 
+                                                  handlePawnPromotion game move
+                                  Left s -> do
+                                              T.write ("Uh-oh: " ++ s ++ "\n") 
+                                              playGame game 
+                                  Right (_, game') -> playGame game' 
 
-                    handlePawnPromotion game move = do
-                        T.write "Enter piece for pawn promotion: \n" 
-                        inputPc <- T.input
-                        case inputPc of
-                          Nothing -> return ()
-                          (Just pawnPromPcInput) -> case (toPiece pawnPromPcInput) of
-                                        Nothing -> do
-                                          T.write "Invalid Piece\n"
-                                          playGame game
-                                        pc -> do
-                                                let game2 = setChessBoardPiece pc game
-                                                case (runStateT (handleTurn move) game2) of
-                                                  Left s -> do
-                                                              T.write ("Uh-oh: " ++ s ++ "\n") 
-                                                              playGame game 
-                                                  Right (_, game') -> do
-                                                                      let game2' =  setChessBoardPiece Nothing game'
-                                                                      playGame game2' 
+  handlePawnPromotion game move = do
+      T.write "Enter piece for pawn promotion: \n" 
+      inputPc <- T.input
+      case inputPc of
+        Nothing -> return ()
+        (Just pawnPromPcInput) -> case (toPiece pawnPromPcInput) of
+                      Nothing -> do
+                        T.write "Invalid Piece\n"
+                        playGame game
+                      pc -> do
+                              let game2 = setChessBoardPiece pc game
+                              case (runStateT (handleTurn move) game2) of
+                                Left s -> do
+                                  T.write ("Uh-oh: " ++ s ++ "\n") 
+                                  playGame game 
+                                Right (_, game') -> do
+                                  let game2' = setChessBoardPiece Nothing game'
+                                  playGame game2' 
 
-                    setChessBoardPiece :: (Maybe PieceType) -> Game -> Game
-                    setChessBoardPiece pc game = case (runStateT (setPromotedPieceInGame pc) game) of
-                                                  Left s -> game
-                                                  Right (_, game') -> game'
+  setChessBoardPiece :: (Maybe PieceType) -> Game -> Game
+  setChessBoardPiece pc game = 
+                            case (runStateT (setPromotedPieceInGame pc) game) of
+                                Left s -> game
+                                Right (_, game') -> game'
              
 -------------------------------------------------------------------------
 
 printLog :: Output m => Game -> m ()
 printLog game = mapM_ T.write (map show (moveLog game))
 
--- this throws exceptions if not a digit - handle that
 getNextMove :: String -> Maybe Move
-getNextMove (a:m:x:b:n:xs) = if (wr a') && (wr b') &&
+getNextMove (a:m:x:b:n:xs) = if validChars a m b n &&
+                                (wr a') && (wr b') &&
                                 (wr m') && (wr n') &&
                                 (x == ',' || isSpace x)
                              then (Just (Move (Loc a' m') (Loc b' n')))
@@ -96,6 +101,15 @@ getNextMove (a:m:x:b:n:xs) = if (wr a') && (wr b') &&
 
                              wr :: Int -> Bool
                              wr i = i > 0 && i < 9
+
+                             validChars :: Char -> Char -> Char -> Char -> Bool
+                             validChars a m b n = ((a >= 'a' && a <= 'h') || 
+                                                   (a >= 'A' && a <= 'H')) && 
+                                                  ((b >= 'a' && b <= 'h') || 
+                                                   (b >= 'A' && b <= 'H')) &&
+                                                  (m >= '1' && m <= '8') && 
+                                                  (n >= '1' && n <= '8')
+
 getNextMove _ = Nothing
 
 toPiece :: String -> (Maybe PieceType)
@@ -106,8 +120,6 @@ toPiece "Rook" = Just Rook
 toPiece "Pawn" = Just Pawn
 toPiece _ = Nothing
 -------------------------------------------------------------------------
-
--- try to do this with foldm / fold
 
 pBoardX :: Output m => Board -> Int -> m ()
 pBoardX b 1 = do
